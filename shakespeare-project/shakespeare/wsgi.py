853182440e5e08a516bf4d75e33b2350fe59bfce
Henry Goddard <godd9170@mylaurier.ca>
2017-02-24 19:08:41 -0500

    trying to get wsgi to recoginize shakespeare again again again

--- shakespeare-project/shakespeare/wsgi.py
+++ shakespeare-project/shakespeare/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shakespeare.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shakespeare-project.shakespeare.settings")
 
 application = get_wsgi_application()
