9523c2c077fe43c6eea178ade4e114d38c79d6e7
Henry Goddard <godd9170@mylaurier.ca>
2017-02-27 15:35:42 -0500

    don't double create su

--- shakespeare/personas/management/commands/createsu.py
+++ shakespeare/personas/management/commands/createsu.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
+        if not User.objects.filter(username="shakespeareadmin").exists():
             User.objects.create_superuser("shakespeareadmin", "hgoddard@saasli.com", "salesforce1")
