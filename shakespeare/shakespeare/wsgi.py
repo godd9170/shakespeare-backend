00fe32dc0f1416de7606e2bc3c1132964af8f835
Henry Goddard <godd9170@mylaurier.ca>
2017-02-26 22:43:30 -0500

    import sys in wsgi

--- shakespeare/shakespeare/wsgi.py
+++ shakespeare/shakespeare/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 sys.path.insert(0, '/opt/python/current/app') #Thanks http://stackoverflow.com/a/26822650/7322725
 
 from django.core.wsgi import get_wsgi_application
