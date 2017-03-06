102659d2725b28ce6e723bcc3fdd7c1e4f993f6a
Henry Goddard <godd9170@mylaurier.ca>
2017-03-06 18:47:44 -0500

    actually make the not found error a 404

--- shakespeare/research/exceptions.py
+++ shakespeare/research/exceptions.py
@@ -1,6 +1,6 @@
 from rest_framework.exceptions import APIException
 
 class ContactNotFoundException(APIException):
-    status_code = 500
+    status_code = 404
     default_detail = 'Unable to find a contact by that email.'
     default_code = 'not_found'
