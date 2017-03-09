f4b3f2c88f02d462503c2291ac40b0551ccccb1b
Henry Goddard <godd9170@mylaurier.ca>
2017-03-08 19:28:11 -0500

    Lower the logging level

--- shakespeare/shakespeare/settings.py
+++ shakespeare/shakespeare/settings.py
@@ -67,7 +67,7 @@ LOGGING = {
     },
     'handlers': {
         'console': {
-            'level': 'DEBUG',
+            'level': 'INFO',
             'class': 'logging.StreamHandler',
             'formatter': 'simple'
         },
