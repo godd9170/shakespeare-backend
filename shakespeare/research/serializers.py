646211124b9f8eb4aa0288b75ffffa998c8bcf6c
Henry Goddard <godd9170@mylaurier.ca>
2017-03-14 09:25:43 -0400

    Attempting to serialize the appropriate Wrappers

--- shakespeare/research/serializers.py
+++ shakespeare/research/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from research.models import Company, Individual, Research, Piece, Nugget
+from research.models import Company, Individual, Research, Piece, Nugget, NuggetWrapper
 from django.contrib.auth.models import User
 
 class CompanySerializer(serializers.ModelSerializer):
@@ -15,11 +15,27 @@ class IndividualSerializer(serializers.ModelSerializer):
         model = Individual
         fields = ( 'email', 'created', 'firstname', 'lastname', 'jobtitle', 'role', 'avatar', 'company', 'companyname')
 
+class NuggetWrapperSerializer(serializers.RelatedField):
+    def get_queryset(self, values):
+        print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SELF: {}'.format(self))
+        print('VALUES: {}'.format(values))
+        # print('KWARGS: {}'.format(kwargs))
+        return NuggetWrapper.objects.filter(mergefields__contained_by=['django'])
+
+    # def to_representation(self, value):
+    #     print('VALUE {}'.format(value))
+    #     return NuggetWrapper.objects.filter(mergefields__contained_by=['django'])
+
+    class Meta:
+        model = NuggetWrapper
+        fields = ('id', 'wrapper')
+
 class NuggetSerializer(serializers.ModelSerializer):
+    wrappers = NuggetWrapperSerializer(many=True)
 
     class Meta:
         model = Nugget
-        fields = ('id', 'created', 'speaker', 'body' )
+        fields = ('id', 'created', 'speaker', 'body', 'wrappers')
 
 class PieceSerializer(serializers.ModelSerializer):
     nuggets = NuggetSerializer(many=True, source='nugget', read_only=True)
