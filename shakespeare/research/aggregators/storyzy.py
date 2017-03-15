f03cf17be072cb4055bb2041354cce00d18163ae
Henry Goddard <godd9170@mylaurier.ca>
2017-03-15 18:15:19 -0400

    move speaker to additional data and get 'from' quotes for storzy

--- shakespeare/research/aggregators/storyzy.py
+++ shakespeare/research/aggregators/storyzy.py
@@ -20,14 +20,20 @@ from research.models import Research, Piece, Nugget
 #         ]
 #     }
 # ]
-def reshape_payload(quotes):
+def reshape_payload(quotes, category):
     research_pieces = []
     for quote in quotes:
         this_source = quote['source']
+        speaker = quote['speakers'][0] ###ASSUMING 1st speak is the only speaker
         nugget = {
             'body' : quote['quote'],
-            'category' : 'quote',
-            'speaker' : quote['speakers'][0]['name'] ###ASSUMING 1st speak is the only speaker
+            'category' : category,
+            'additionaldata' : {
+                'speakername' : speaker['name'],
+                'speakercompany' : speaker['from'],
+                'speakertype' : speaker['type'],
+                'speakerpublisher' : speaker['publisher']
+            }
         }
         this_research_piece = list(filter(lambda x: x.get('source_id') == this_source['id'], research_pieces)) #filter out all the elements that don't have that source id
         if len(this_research_piece) == 0: #this source hasn't showed up yet, let's make a new piece
@@ -36,7 +42,8 @@ def reshape_payload(quotes):
                 'title' : this_source['title'],
                 'url' : this_source['uri'],
                 'publisheddate' : datetime.datetime.fromtimestamp(int(quote['date']/1000)),
-                'nuggets' : [nugget]
+                'nuggets' : [nugget],
+                'source' : this_source
             })
         else: #we've already created the source, grab it
             this_research_piece[0]['nuggets'].append(nugget)
@@ -47,14 +54,14 @@ def do_storyzy(research):
     url = "http://www.storyzy.com/searchData?q={}".format(companyName) #get 
     response = requests.get(url).json()
     # obtain an array of quotes
-    research_pieces = reshape_payload(response['searchResponse']['quotesAbout'])
+    research_pieces = reshape_payload(response['searchResponse']['quotesAbout'], 'quote_about') + reshape_payload(response['searchResponse']['quotesFrom'], 'quote_from')
     for piece in research_pieces:
         piece.pop('source_id', None) # source_id is no longer necessary
         nuggets = piece.pop('nuggets', None) # get the nugget array
         newPiece = Piece(research=research, **piece)
-        newPiece.save()
+        newPiece.save() #.full_clean()
         for nugget in nuggets:
-            Nugget(piece=newPiece, **nugget).save() #Make the new nugget
+            Nugget(piece=newPiece, **nugget).save() #.full_clean()
 
     
 
