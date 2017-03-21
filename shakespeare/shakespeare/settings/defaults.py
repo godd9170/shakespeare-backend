634a0772b49b15262e220c4a2025d557e3335005
Henry Goddard <godd9170@mylaurier.ca>
2017-03-21 16:20:25 -0400

    updating clearbit again

--- shakespeare/shakespeare/settings/defaults.py
+++ shakespeare/shakespeare/settings/defaults.py
@@ -264,4 +264,5 @@ BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
 CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 
 # CLEARBIT_KEY = 'sk_886efa2d89a51d9fc048d5d04023d09a' # Henry's first account
-CLEARBIT_KEY = 'sk_a1bf7f2daff22c076f07080a660ebddd' # Richard's first account
+# CLEARBIT_KEY = 'sk_a1bf7f2daff22c076f07080a660ebddd' # Richard's first account
+CLEARBIT_KEY = 'sk_e4f319623ed71b0d786865a8d4184c5d'
