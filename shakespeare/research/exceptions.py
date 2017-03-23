5bcd227197bfc9d899288a831ef9ba46994d1d7e
Henry Goddard <godd9170@mylaurier.ca>
2017-03-22 22:55:18 -0400

    nitpick

--- shakespeare/research/exceptions.py
+++ shakespeare/research/exceptions.py
@@ -1,17 +1,14 @@
 from rest_framework.exceptions import APIException
 
-
 class ContactNotFoundException(APIException):
     status_code = 404
     default_detail = 'Unable to find a contact by that email.'
     default_code = 'not_found'
 
-
 class UnexpectedClearbitPersonPayload(APIException):
     status_code = 500
     default_detail = 'Unexpected error on person search.'
     default_code = 'internal_error'
-
 
 class UnexpectedClearbitCompanyPayload(APIException):
     status_code = 500
