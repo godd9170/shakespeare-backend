fbd64c46c1f98dc3f2616c803baf3a7566532c16
George-Bogdan Ivanov <ivanov.george.bogdan@gmail.com>
2017-04-05 15:28:31 +0100

    imports to fix

--- shakespeare/research/aggregators/__init__.py
+++ shakespeare/research/aggregators/__init__.py
@@ -1,1 +1,6 @@
-__all__ = ['aggregator', 'featuredcustomers', 'storyzy', 'predictleads']
+# __all__ = ['aggregator', 'featuredcustomers', 'storyzy', 'predictleads']
+
+# from .aggregator import *
+# from .featuredcustomers import *
+# from .storyzy import *
+# from .predictleads import *
