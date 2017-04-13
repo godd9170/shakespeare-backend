742846b1338a6fa38d4cc670db4d877d4883c73f
Henry Goddard <godd9170@mylaurier.ca>
2017-04-13 11:09:48 -0400

    roller deploy unknown

--- .ebextensions/swap/setup_swap.sh
+++ .ebextensions/swap/setup_swap.sh
@@ -3,15 +3,11 @@
 SWAPFILE=/var/swapfile
 SWAP_MEGABYTES=2048
 
-/usr/bin/curl -X POST -d "place=b4if" http://requestb.in/pdl449pd
-
 if [ -f $SWAPFILE ]; then
 	echo "Swapfile $SWAPFILE found, assuming already setup"
-	/usr/bin/curl -X POST -d "place=insideif" http://requestb.in/pdl449pd
 	exit;
 fi
 
-/usr/bin/curl -X POST -d "place=afterif" http://requestb.in/pdl449pd
 
 /bin/dd if=/dev/zero of=$SWAPFILE bs=1M count=$SWAP_MEGABYTES
 /bin/chmod 600 $SWAPFILE
