8e2ebf8679f780e5b45e16beb0c1f3ea94924f5b
Henry Goddard <godd9170@mylaurier.ca>
2017-04-13 10:32:25 -0400

    testing swapfile changes

--- .ebextensions/swap/setup_swap.sh
+++ .ebextensions/swap/setup_swap.sh
@@ -5,8 +5,11 @@ SWAP_MEGABYTES=2048
 
 if [ -f $SWAPFILE ]; then
 	echo "Swapfile $SWAPFILE found, assuming already setup"
+	/usr/bin/curl -X POST -d "place=insideif" http://requestb.in/pdl449pd
 	exit;
 fi
+
+/usr/bin/curl -X POST -d "place=outsideif" http://requestb.in/pdl449pd
 
 /bin/dd if=/dev/zero of=$SWAPFILE bs=1M count=$SWAP_MEGABYTES
 /bin/chmod 600 $SWAPFILE
