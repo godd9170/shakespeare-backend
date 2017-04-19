cddbe978ecc2b7871196a7521106aa9d5e90b1ad
Henry Goddard <godd9170@mylaurier.ca>
2017-04-19 11:14:30 -0400

    comment out the rollbar config in dev settings

--- shakespeare/shakespeare/settings/dev.py
+++ shakespeare/shakespeare/settings/dev.py
@@ -81,6 +81,15 @@ LOGGING = {
     }
 }
 
+# MIDDLEWARE.append('rollbar.contrib.django.middleware.RollbarNotifierMiddleware')
+# ROLLBAR = {
+#     'access_token': '38b1269535074f1a9b4c7da6a8142502',
+#     'environment': 'local',
+#     'root': BASE_DIR,
+# }
+# import rollbar
+# rollbar.init(**ROLLBAR)
+
 # -------------
 # Celery
 # -------------
