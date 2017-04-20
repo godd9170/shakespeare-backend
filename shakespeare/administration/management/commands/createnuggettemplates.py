1e0701f229560cec9c55d2b56d1a1506fc6f7777
Henry Goddard <godd9170@mylaurier.ca>
2017-04-20 15:00:54 -0400

    alter nugget creation mgmt script to check for the existence of a template before trying to create it.

--- shakespeare/administration/management/commands/createnuggettemplates.py
+++ shakespeare/administration/management/commands/createnuggettemplates.py
@@ -6,6 +6,9 @@ from .data.nuggettemplates import TEMPLATES
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if (NuggetTemplate.objects.all().count() < 1):
-            for nugget in TEMPLATES:
+        for nugget in TEMPLATES:
+            #check to see if the template already exists. #NOTE: Any small changes made within the admin
+            #console will be enough to warrant a new nugget template creation. 
+            if (NuggetTemplate.objects.filter(segue=nugget['segue'], intro=nugget['intro'], subject=nugget['subject']).count() < 1):
+                print('Creating Template: {}'.format(nugget['subject']))
                 NuggetTemplate(**nugget).save()
