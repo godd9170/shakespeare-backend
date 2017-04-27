f20902af5f7bf094210961c0ce404b87be29f4d7
Henry Goddard <godd9170@mylaurier.ca>
2017-04-26 22:02:20 -0400

    Re-format some research admin model views

--- shakespeare/research/admin.py
+++ shakespeare/research/admin.py
@@ -16,8 +16,33 @@ class IndividualAdmin(admin.ModelAdmin):
             return u'<a href="%s">%s</a>' % (link,obj.company)
     link_company.allow_tags = True
 
-admin.site.register(Company)
-admin.site.register(Research)
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('domain', 'name', 'sector')
+    list_display_links = ('domain',)
+    search_fields = ('domain', 'name')
+    list_per_page = 20
+
+@admin.register(Research)
+class ResearchAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'link_individual', 'piece_count')
+    list_display_links = ('id',)
+    search_fields = ('owner__name', 'individual__name')
+    list_per_page = 20
+
+    # Cross Object Links
+    def link_individual(self, obj):
+        if obj.individual_id is not None:
+            link=urlresolvers.reverse("admin:research_individual_change", args=[obj.individual_id]) #model name has to be lowercase
+            return u'<a href="%s">%s</a>' % (link,obj.individual)
+    link_individual.allow_tags = True
+
+        # Cross Object Links
+    def piece_count(self, obj):
+        return Piece.objects.filter(research=obj.id).count()
+
+
 admin.site.register(Piece)
 admin.site.register(Nugget)
 admin.site.register(NuggetTemplate)
