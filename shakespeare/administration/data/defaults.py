36fe867b1bbd33efc6b5fe792b1022b8ddc83318
Henry Goddard <godd9170@mylaurier.ca>
2017-05-03 13:22:09 -0400

    Defaults

---
+++ shakespeare/administration/data/defaults.py
@@ -0,0 +1,29 @@
+DEFAULT_VALUE_PROPOSITIONS = [
+	{
+		"title" : "Default VP",
+		"body" : "You should purchase the product I am attempting to sell to you."
+	}
+]
+
+DEFAULT_CALLS_TO_ACTION = [
+    {
+        "title" : "Sponsorship South - Formal",
+        "body" : "Who would be the best person at your company to speak to on this matter?"
+    },
+    {
+        "title" : "Sponsorship South - Casual",
+        "body" : "Can you connect me with the person on your team that would care about solving this problem?"
+    },
+    {
+        "title" : "Soft - 15 min",
+        "body" : "Can we find 15 min to see if it makes sense to keep talking after that?"
+    },
+    {
+        "title" : "Soft - 30 min",
+        "body" : "Can we find 30 min to see if it makes sense to keep talking after that?"
+    },
+    {
+        "title" : "Hard - 15 min",
+        "body" : "I'd like to set up a 15 min call with you - when are you free to chat?"
+    }
+]
